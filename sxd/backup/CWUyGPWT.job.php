28ad7b8db90db6f8fb9e662eba2afa25458914b4
ggrenuy <serjsv87@gmail.com>
2015-06-30 22:26:10 +0300

    ddd

---
+++ sxd/backup/CWUyGPWT.job.php
@@ -0,0 +1,753 @@
+<?php
+$JOB = array (
+  'act' => 'restore',
+  'type' => 'run',
+  'db' => 'drupal',
+  'charset' => '0',
+  'file' => 'drupal_2015-06-30_22-18-14.sql.gz',
+  'strategy' => '0',
+  'correct' => '0',
+  'autoinc' => '0',
+  'obj' => 
+  array (
+    'TA' => 
+    array (
+      0 => '*',
+    ),
+  ),
+  'job' => 'CWUyGPWT',
+  'file_ext' => 'sql.gz',
+  'file_name' => 'backup/drupal_2015-06-30_22-18-14.sql.gz',
+  'file_tmp' => 'backup/drupal_2015-06-30_22-18-14.sql.gz',
+  'file_rtl' => 'backup/CWUyGPWT.rtl',
+  'file_log' => 'backup/CWUyGPWT.log',
+  'file_stp' => 'backup/CWUyGPWT.stp',
+  'todo' => 
+  array (
+    'TA' => 
+    array (
+      0 => 
+      array (
+        0 => 'TA',
+        1 => 'actions',
+        2 => '12',
+        3 => '16384',
+      ),
+      1 => 
+      array (
+        0 => 'TC',
+        1 => 'authmap',
+        2 => '0',
+        3 => '16384',
+      ),
+      2 => 
+      array (
+        0 => 'TC',
+        1 => 'batch',
+        2 => '0',
+        3 => '16384',
+      ),
+      3 => 
+      array (
+        0 => 'TA',
+        1 => 'block',
+        2 => '196',
+        3 => '16384',
+      ),
+      4 => 
+      array (
+        0 => 'TC',
+        1 => 'block_custom',
+        2 => '0',
+        3 => '16384',
+      ),
+      5 => 
+      array (
+        0 => 'TC',
+        1 => 'block_node_type',
+        2 => '0',
+        3 => '16384',
+      ),
+      6 => 
+      array (
+        0 => 'TC',
+        1 => 'block_role',
+        2 => '0',
+        3 => '16384',
+      ),
+      7 => 
+      array (
+        0 => 'TC',
+        1 => 'blocked_ips',
+        2 => '0',
+        3 => '16384',
+      ),
+      8 => 
+      array (
+        0 => 'TA',
+        1 => 'cache',
+        2 => '17',
+        3 => '1425408',
+      ),
+      9 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_admin_menu',
+        2 => '1',
+        3 => '16384',
+      ),
+      10 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_block',
+        2 => '0',
+        3 => '16384',
+      ),
+      11 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_bootstrap',
+        2 => '6',
+        3 => '163840',
+      ),
+      12 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_field',
+        2 => '13',
+        3 => '65536',
+      ),
+      13 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_filter',
+        2 => '0',
+        3 => '16384',
+      ),
+      14 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_form',
+        2 => '0',
+        3 => '16384',
+      ),
+      15 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_image',
+        2 => '0',
+        3 => '16384',
+      ),
+      16 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_l10n_update',
+        2 => '0',
+        3 => '16384',
+      ),
+      17 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_menu',
+        2 => '29',
+        3 => '1589248',
+      ),
+      18 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_page',
+        2 => '1',
+        3 => '16384',
+      ),
+      19 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_path',
+        2 => '0',
+        3 => '16384',
+      ),
+      20 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_update',
+        2 => '19',
+        3 => '32768',
+      ),
+      21 => 
+      array (
+        0 => 'TA',
+        1 => 'cache_views',
+        2 => '22',
+        3 => '245760',
+      ),
+      22 => 
+      array (
+        0 => 'TC',
+        1 => 'cache_views_data',
+        2 => '0',
+        3 => '16384',
+      ),
+      23 => 
+      array (
+        0 => 'TC',
+        1 => 'comment',
+        2 => '0',
+        3 => '16384',
+      ),
+      24 => 
+      array (
+        0 => 'TA',
+        1 => 'context',
+        2 => '2',
+        3 => '16384',
+      ),
+      25 => 
+      array (
+        0 => 'TC',
+        1 => 'ctools_css_cache',
+        2 => '0',
+        3 => '16384',
+      ),
+      26 => 
+      array (
+        0 => 'TC',
+        1 => 'ctools_object_cache',
+        2 => '0',
+        3 => '16384',
+      ),
+      27 => 
+      array (
+        0 => 'TC',
+        1 => 'date_format_locale',
+        2 => '0',
+        3 => '16384',
+      ),
+      28 => 
+      array (
+        0 => 'TA',
+        1 => 'date_format_type',
+        2 => '3',
+        3 => '16384',
+      ),
+      29 => 
+      array (
+        0 => 'TA',
+        1 => 'date_formats',
+        2 => '35',
+        3 => '16384',
+      ),
+      30 => 
+      array (
+        0 => 'TA',
+        1 => 'field_config',
+        2 => '4',
+        3 => '16384',
+      ),
+      31 => 
+      array (
+        0 => 'TA',
+        1 => 'field_config_instance',
+        2 => '8',
+        3 => '16384',
+      ),
+      32 => 
+      array (
+        0 => 'TC',
+        1 => 'field_data_body',
+        2 => '0',
+        3 => '16384',
+      ),
+      33 => 
+      array (
+        0 => 'TC',
+        1 => 'field_data_comment_body',
+        2 => '0',
+        3 => '16384',
+      ),
+      34 => 
+      array (
+        0 => 'TC',
+        1 => 'field_data_field_image',
+        2 => '0',
+        3 => '16384',
+      ),
+      35 => 
+      array (
+        0 => 'TC',
+        1 => 'field_data_field_tags',
+        2 => '0',
+        3 => '16384',
+      ),
+      36 => 
+      array (
+        0 => 'TC',
+        1 => 'field_revision_body',
+        2 => '0',
+        3 => '16384',
+      ),
+      37 => 
+      array (
+        0 => 'TC',
+        1 => 'field_revision_comment_body',
+        2 => '0',
+        3 => '16384',
+      ),
+      38 => 
+      array (
+        0 => 'TC',
+        1 => 'field_revision_field_image',
+        2 => '0',
+        3 => '16384',
+      ),
+      39 => 
+      array (
+        0 => 'TC',
+        1 => 'field_revision_field_tags',
+        2 => '0',
+        3 => '16384',
+      ),
+      40 => 
+      array (
+        0 => 'TA',
+        1 => 'file_managed',
+        2 => '1',
+        3 => '16384',
+      ),
+      41 => 
+      array (
+        0 => 'TC',
+        1 => 'file_usage',
+        2 => '0',
+        3 => '16384',
+      ),
+      42 => 
+      array (
+        0 => 'TA',
+        1 => 'filter',
+        2 => '15',
+        3 => '16384',
+      ),
+      43 => 
+      array (
+        0 => 'TA',
+        1 => 'filter_format',
+        2 => '3',
+        3 => '16384',
+      ),
+      44 => 
+      array (
+        0 => 'TC',
+        1 => 'flood',
+        2 => '0',
+        3 => '16384',
+      ),
+      45 => 
+      array (
+        0 => 'TC',
+        1 => 'history',
+        2 => '0',
+        3 => '16384',
+      ),
+      46 => 
+      array (
+        0 => 'TC',
+        1 => 'image_effects',
+        2 => '0',
+        3 => '16384',
+      ),
+      47 => 
+      array (
+        0 => 'TC',
+        1 => 'image_styles',
+        2 => '0',
+        3 => '16384',
+      ),
+      48 => 
+      array (
+        0 => 'TA',
+        1 => 'l10n_update_file',
+        2 => '16',
+        3 => '16384',
+      ),
+      49 => 
+      array (
+        0 => 'TA',
+        1 => 'l10n_update_project',
+        2 => '18',
+        3 => '16384',
+      ),
+      50 => 
+      array (
+        0 => 'TA',
+        1 => 'languages',
+        2 => '2',
+        3 => '16384',
+      ),
+      51 => 
+      array (
+        0 => 'TA',
+        1 => 'locales_source',
+        2 => '9172',
+        3 => '1589248',
+      ),
+      52 => 
+      array (
+        0 => 'TA',
+        1 => 'locales_target',
+        2 => '7892',
+        3 => '1589248',
+      ),
+      53 => 
+      array (
+        0 => 'TA',
+        1 => 'menu_custom',
+        2 => '6',
+        3 => '16384',
+      ),
+      54 => 
+      array (
+        0 => 'TA',
+        1 => 'menu_links',
+        2 => '426',
+        3 => '114688',
+      ),
+      55 => 
+      array (
+        0 => 'TA',
+        1 => 'menu_router',
+        2 => '470',
+        3 => '409600',
+      ),
+      56 => 
+      array (
+        0 => 'TA',
+        1 => 'node',
+        2 => '1',
+        3 => '16384',
+      ),
+      57 => 
+      array (
+        0 => 'TA',
+        1 => 'node_access',
+        2 => '1',
+        3 => '16384',
+      ),
+      58 => 
+      array (
+        0 => 'TC',
+        1 => 'node_comment_statistics',
+        2 => '0',
+        3 => '16384',
+      ),
+      59 => 
+      array (
+        0 => 'TC',
+        1 => 'node_revision',
+        2 => '0',
+        3 => '16384',
+      ),
+      60 => 
+      array (
+        0 => 'TA',
+        1 => 'node_type',
+        2 => '3',
+        3 => '16384',
+      ),
+      61 => 
+      array (
+        0 => 'TC',
+        1 => 'nodequeue_nodes',
+        2 => '0',
+        3 => '16384',
+      ),
+      62 => 
+      array (
+        0 => 'TC',
+        1 => 'nodequeue_queue',
+        2 => '0',
+        3 => '16384',
+      ),
+      63 => 
+      array (
+        0 => 'TC',
+        1 => 'nodequeue_roles',
+        2 => '0',
+        3 => '16384',
+      ),
+      64 => 
+      array (
+        0 => 'TC',
+        1 => 'nodequeue_subqueue',
+        2 => '0',
+        3 => '16384',
+      ),
+      65 => 
+      array (
+        0 => 'TC',
+        1 => 'nodequeue_types',
+        2 => '0',
+        3 => '16384',
+      ),
+      66 => 
+      array (
+        0 => 'TA',
+        1 => 'queue',
+        2 => '18',
+        3 => '16384',
+      ),
+      67 => 
+      array (
+        0 => 'TA',
+        1 => 'rdf_mapping',
+        2 => '3',
+        3 => '16384',
+      ),
+      68 => 
+      array (
+        0 => 'TA',
+        1 => 'registry',
+        2 => '812',
+        3 => '212992',
+      ),
+      69 => 
+      array (
+        0 => 'TA',
+        1 => 'registry_file',
+        2 => '500',
+        3 => '180224',
+      ),
+      70 => 
+      array (
+        0 => 'TA',
+        1 => 'role',
+        2 => '3',
+        3 => '16384',
+      ),
+      71 => 
+      array (
+        0 => 'TA',
+        1 => 'role_permission',
+        2 => '100',
+        3 => '16384',
+      ),
+      72 => 
+      array (
+        0 => 'TC',
+        1 => 'search_dataset',
+        2 => '0',
+        3 => '16384',
+      ),
+      73 => 
+      array (
+        0 => 'TA',
+        1 => 'search_index',
+        2 => '2',
+        3 => '16384',
+      ),
+      74 => 
+      array (
+        0 => 'TC',
+        1 => 'search_node_links',
+        2 => '0',
+        3 => '16384',
+      ),
+      75 => 
+      array (
+        0 => 'TA',
+        1 => 'search_total',
+        2 => '2',
+        3 => '16384',
+      ),
+      76 => 
+      array (
+        0 => 'TC',
+        1 => 'semaphore',
+        2 => '0',
+        3 => '16384',
+      ),
+      77 => 
+      array (
+        0 => 'TA',
+        1 => 'sequences',
+        2 => '1',
+        3 => '16384',
+      ),
+      78 => 
+      array (
+        0 => 'TA',
+        1 => 'sessions',
+        2 => '1',
+        3 => '16384',
+      ),
+      79 => 
+      array (
+        0 => 'TA',
+        1 => 'shortcut_set',
+        2 => '1',
+        3 => '16384',
+      ),
+      80 => 
+      array (
+        0 => 'TC',
+        1 => 'shortcut_set_users',
+        2 => '0',
+        3 => '16384',
+      ),
+      81 => 
+      array (
+        0 => 'TA',
+        1 => 'system',
+        2 => '210',
+        3 => '229376',
+      ),
+      82 => 
+      array (
+        0 => 'TC',
+        1 => 'taxonomy_index',
+        2 => '0',
+        3 => '16384',
+      ),
+      83 => 
+      array (
+        0 => 'TC',
+        1 => 'taxonomy_term_data',
+        2 => '0',
+        3 => '16384',
+      ),
+      84 => 
+      array (
+        0 => 'TC',
+        1 => 'taxonomy_term_hierarchy',
+        2 => '0',
+        3 => '16384',
+      ),
+      85 => 
+      array (
+        0 => 'TA',
+        1 => 'taxonomy_vocabulary',
+        2 => '1',
+        3 => '16384',
+      ),
+      86 => 
+      array (
+        0 => 'TC',
+        1 => 'url_alias',
+        2 => '0',
+        3 => '16384',
+      ),
+      87 => 
+      array (
+        0 => 'TA',
+        1 => 'users',
+        2 => '4',
+        3 => '16384',
+      ),
+      88 => 
+      array (
+        0 => 'TA',
+        1 => 'users_roles',
+        2 => '1',
+        3 => '16384',
+      ),
+      89 => 
+      array (
+        0 => 'TA',
+        1 => 'variable',
+        2 => '137',
+        3 => '49152',
+      ),
+      90 => 
+      array (
+        0 => 'TC',
+        1 => 'views_display',
+        2 => '0',
+        3 => '16384',
+      ),
+      91 => 
+      array (
+        0 => 'TC',
+        1 => 'views_view',
+        2 => '0',
+        3 => '16384',
+      ),
+      92 => 
+      array (
+        0 => 'TA',
+        1 => 'watchdog',
+        2 => '487',
+        3 => '180224',
+      ),
+      93 => 
+      array (
+        0 => 'TC',
+        1 => 'webform',
+        2 => '0',
+        3 => '16384',
+      ),
+      94 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_component',
+        2 => '0',
+        3 => '16384',
+      ),
+      95 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_conditional',
+        2 => '0',
+        3 => '16384',
+      ),
+      96 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_conditional_actions',
+        2 => '0',
+        3 => '16384',
+      ),
+      97 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_conditional_rules',
+        2 => '0',
+        3 => '16384',
+      ),
+      98 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_emails',
+        2 => '0',
+        3 => '16384',
+      ),
+      99 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_last_download',
+        2 => '0',
+        3 => '16384',
+      ),
+      100 => 
+      array (
+        0 => 'TA',
+        1 => 'webform_roles',
+        2 => '2',
+        3 => '16384',
+      ),
+      101 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_submissions',
+        2 => '0',
+        3 => '16384',
+      ),
+      102 => 
+      array (
+        0 => 'TC',
+        1 => 'webform_submitted_data',
+        2 => '0',
+        3 => '16384',
+      ),
+    ),
+  ),
+);
+?>
